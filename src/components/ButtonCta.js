e2da6276aa37cc754e2ef5420c3ebc73a615c7a6
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2023-01-22 13:52:01 -0600

    PROMO-111 ~ responsice on mobile

--- src/components/ButtonCta.js
+++ src/components/ButtonCta.js
@@ -1,7 +1,7 @@
 const ButtonCta = ({size, text='Get Started'}) => {
     const handleCtaClick = () => alert('we are no longer accepting users. we are dedicated to the development of problem solving tools. niniwiseoriginal@gmail.com, olufemiaf@gmail.com')
     return (
-        <div className={`button__cta ${size}`}>
+        <div className={`button__cta ${size ? size : ''}`}>
             <button onClick={handleCtaClick} className="btn">
                 {text}
             </button>
