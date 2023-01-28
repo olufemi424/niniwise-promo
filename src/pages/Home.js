aeb1ef62342a7073750551ae8eead7894e4fb574
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2023-01-28 00:58:43 -0600

    PROMO-114 ~ exploring new home hero section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,51 @@
 // import niniwiseImage1 from "../assets/app-home.png";
 import niniwiseImage1 from "../assets/app-home1.png";
-import backgroundImage from "../assets/backgrounds/home-hero.jpeg";
 import ButtonCta from "../components/ButtonCta";
-import HomeHero from "../components/HeroSection";
 import Benefits from "../components/Benefits";
 import Footer from "../components/Footer";
 import ReviewCard from "../components/ReviewCard";
 
 const Home = () => {
-    const homeHeroData = {
-        backgroundImage: backgroundImage,
-        title: "Journal  your way to consistent profitability -",
-        description: "A trading journal is a useful tool for traders to track their progress, identify patterns and mistakes, and improve their strategies. It can help traders overcome biases and emotions and improve their performance in the market."
-    }
     return (
         <main className="home">
-            <HomeHero
+            {/* <HomeHero
                 background={homeHeroData.backgroundImage}
                 title={homeHeroData.title}
                 description={homeHeroData.description}
                 getStartedButtonisEnabled={true}
-            />
+            /> */}
+            <section className="home-landing">
+                <div className="container">
+                    <div className="home-landing__info">
+                        <h2 className="home-landing__info-title">
+                            Journal your way to consistent profitability -
+                        </h2>
+                        <p className="home-landing__info-text">
+                            A trading journal is a useful tool for traders to track their progress, identify patterns and mistakes, and improve their strategies. It can help traders overcome biases and emotions and improve their performance in the market.
+                        </p>
+                         <ButtonCta />
+                    </div>
+                    <div className="home-landing__info">
+                        <div className="home-landing__img-container">
+                            <img
+                                className="home-landing-img image-1"
+                                src={ niniwiseImage1 }
+                                alt="Niniwise journal"
+                            />
+                            <img
+                                className="home-landing-img image-2"
+                                src={ niniwiseImage1 }
+                                alt="Niniwise journal"
+                            />
+                            <img
+                                className="home-landing-img image-3"
+                                src={ niniwiseImage1 }
+                                alt="Niniwise journal"
+                            />
+                        </div>
+                    </div>
+                </div>
+            </section>
             <div className="home__img-container">
                 <img
                     className="home__img-image"
