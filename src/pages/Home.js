e888a7f4f93210fca19d47b1542c0a2d52df8747
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2023-02-04 23:31:17 -0600

    PROMO-114 ~ html semantic

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,7 +14,7 @@ const Home = () => {
     }
     return (
         <main className="home">
-            <div className="home-hero-mobile">
+            <section className="home-hero-mobile">
                 <HomeHero
                     className="home-hero-mobile"
                     background={homeHeroData.backgroundImage}
@@ -22,15 +22,15 @@ const Home = () => {
                     description={homeHeroData.description}
                     getStartedButtonisEnabled={true}
                 />
-            </div>
+            </section>
             <section className="home-landing">
                 <div className="container">
                     <div className="home-landing__info">
                         <h2 className="home-landing__info-title">
-                            Journal your way to consistent profitability -
+                            {homeHeroData.title}
                         </h2>
                         <p className="home-landing__info-text">
-                            A trading journal is a useful tool for traders to track their progress, identify patterns and mistakes, and improve their strategies. It can help traders overcome biases and emotions and improve their performance in the market.
+                           {homeHeroData.description}
                         </p>
                          <ButtonCta />
                     </div>
@@ -55,20 +55,20 @@ const Home = () => {
                     </div>
                 </div>
             </section>
-            <div className="home__img-container">
+            <section className="home__img-container">
                 <img
                     className="home__img-image"
                     src={ niniwiseImage1 }
                     alt="Niniwise journal"
                 />
-            </div>
+            </section>
             <Benefits/>
-            <div className="cta-section">
+            <section className="cta-section">
                 <ButtonCta/>
-            </div>
-            <div className="user-reviews">
+            </section>
+            <section className="user-reviews">
                 <ReviewCard/>
-            </div>
+            </section>
             <Footer></Footer>
         </main>
     );
